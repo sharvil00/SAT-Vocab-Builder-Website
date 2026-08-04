f2232d055d8e8e921fb0dd875a62a213e5b7c7e3
sharvil00 <sharvil.shetye@gmail.com>
2026-08-04 09:50:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
 
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'light') {
+    body.classList.add('light-mode');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+
+    if (body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
